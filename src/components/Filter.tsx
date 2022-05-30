b547f8649208faba34ac06cb18b2c120f259b481
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-05-30 06:13:52 +0200

    wip

---
+++ src/components/Filter.tsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Filter = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={9}></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Filter
